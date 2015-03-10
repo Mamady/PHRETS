cff0cf9452ee1929d4c6ded961b30b76941146d2
Mohammad <m@forouzani.com>
2015-03-09 19:39:43 -0700

    WIP - moving code to be usable across different major markets

--- run.php
+++ run.php
@@ -2,72 +2,141 @@
 
 require_once('phrets.php');
 
-$login = 'http://sef.rets.interealty.com/Login.asmx/Login';
-$username = 'Mperaltars';
-$password = 'ertb2942';
+// define connections for each major market
+$major_markets = [
+  'hawaii' => [
+    'login'    => 'http://matrixrets.hicentralmls.com/rets/login.ashx',
+    'username' => 'JtsmitH02',
+    'password' => 'X8y8H65X',
+  ],
 
-$login = 'http://glvar.apps.retsiq.com/rets/login';
-$username = 'koc';
-$password = 'rets';
+  'las_vegas' => [
+    'login'    => 'http://glvar.apps.retsiq.com/rets/login',
+    'username' => 'koc',
+    'password' => 'rets',
+  ],
 
+  'miami' => [
+    'login'    => 'http://sef.rets.interealty.com/Login.asmx/Login',
+    'username' => 'MpeRaltars',
+    'password' => 'ertb2942',
+    'uapassword' => '123456',
+  ],
+];
 
-// start rets connection
-$rets = new phRETS;
+foreach ($major_markets as $major_market  => $connection_data) {
+  $login = $connection_data['login'];
+  $username = $connection_data['username'];
+  $password = $connection_data['password'];
 
-// Uncomment and change the following if you're connecting to a server that supports a version other than RETS 1.5
-//$rets->AddHeader("RETS-Version", "RETS/1.7.2");
+  // start rets connection
+  $rets = new phRETS;
 
-echo "+ Connecting to {$login} as {$username}<br>\n";
-$connect = $rets->Connect($login, $username, $password);
+  $rets->AddHeader("User-Agent", 'RETS-Connector/1.2');
 
-// check for errors
-if ($connect) {
-        echo "  + Connected<br>\n";
-}
-else {
-        echo "  + Not connected:<br>\n";
-        var_dump($rets->Error());
-        exit;
-}
+  // Uncomment and change the following if you're connecting to a server that supports a version other than RETS 1.5
+  //$rets->AddHeader("RETS-Version", "RETS/1.7.2");
 
-$types = $rets->GetMetadataTypes();
+  echo "+ Connecting to {$login} as {$username}\n";
+  if (array_key_exists('uapassword', $connection_data)) {
+    $connection = $rets->Connect($login, $username, $password, $connection_data['uapassword']);
+  } else {
+    $connection = $rets->Connect($login, $username, $password);
+  }
 
-// check for errors
-if (!$types) {
-        print_r($rets->Error());
-}
-else {
-  echo "Running Search ... \n";
-  //var_dump($types);
-  $search_options = array(
-    "Limit" => "2",
-    "Select" => "sysid",
-    //"Select" => "StreetNumber,StreetName,ListPrice,City,StateOrProvince,PostalCode",
-  );
-
-  //var_dump($rets->GetMetadataTypes()); exit;
-  //print_r($rets->GetMetadataClasses("Property")); exit;
-  //var_dump($rets->GetMetadataTable("Property", 4)); exit;
-  $limit = 100;
-  $offset = 1;
-  while ( $limit >= $offset ) {
-    echo "Paginating... \n";
-    $search = $rets->SearchQuery("Property", 1, "(104=2000-01-01+)", $search_options);
-    var_dump($rets->TotalRecordsFound()); exit;
-
+  // check for errors
+  if ($connection) {
+    //if($major_market == 'las_vegas' || $major_market == 'hawaii') { continue; }
+    echo "  + Connected\n";
+  } else {
+    echo "  + Not connected:\n";
     var_dump($rets->Error());
+    continue;
+  }
+
+  $meta_data_types = $rets->GetMetadataTypes();
+  echo 'aa';
+  exit;
+
+  //// filter out everything other than property
+  //foreach ($meta_data_types as $data_type) {
+    //if $data_type[
+  //}
+
+
+
+  $property_types = [
+    "single_family" => 1,
+  ];
+
+  foreach($property_types as $property_type_name => $property_type_id) {
+    generate_csv($rets, $major_market, $property_type_name, $property_type_id);
+  }
+
+  function generate_csv($rets, $major_market, $type_name, $type_id) {
+
+    //// check for errors
+    //if (!$types) {
+      //print_r($rets->Error());
+    //}
+      //var_dump($types);
+    echo "Running Search ... ";
+    //$searching = true;
+    //while ($searching) {
+    //}
+    //echo "Running Search ... ";
+    $search_options = array(
+      //"Limit" => "NONE",
+      "Limit" => 2,
+      "Select" => "sysid",
+      //"Select" => "StreetNumber,StreetName,ListPrice,City,StateOrProvince,PostalCode",
+    );
+
+      //var_dump($rets->GetMetadataTypes()); exit;
+      //print_r($rets->GetMetadataClasses("Property")); exit;
+      //var_dump($rets->GetMetadataTable("Property", 4)); exit;
+
+    // time the search query
+    $time_start = microtime(true);
+
+    // select all properties
+    $search = $rets->SearchQuery("Property", $type_id, "(sysid=0+)", $search_options);
+
+    $time_end = microtime(true);
+    $total_time = $time_end - $time_start;
+
+    echo "  + Search took $total_time seconds and found {$rets->TotalRecordsFound()} records\n";
+
+
+    $file_name = "$type_name.csv";
+    $fh = fopen("data/$major_market/".$file_name, "w+");
+
+    echo 'Displaying results...';
+    $counter = 1;
     while($listing = $rets->FetchRow($search)) {
-      echo "Address: {$listing['StreetNumber']} {$listing['StreetName']}, ";
-      echo "{$listing['City']}, ";
-      echo "{$listing['StateOrProvince']} {$listing['PostalCode']} listed for ";
-      echo "\$".number_format($listing['ListPrice'])."\n";
+
+
+      $search2 = $rets->SearchQuery("Property", 1, "(sysid={$listing['sysid']})", ["Limit" => 1]);
+
+      if($counter == 1) {
+        // print headers
+        $fields = $rets->SearchGetFields($search2);
+        fputcsv($fh, $fields);
+      }
+
+      $property = $rets->FetchRow($search2);
+      fputcsv($fh, $property);
+
+      $counter++;
+      flush();
     }
 
-    $offset = $offset + $limit;
-  }
-  $rets->FreeResult($search);
-}
+    $rets->FreeResult($search);
+    $rets->FreeResult($search2);
 
-echo "+ Disconnecting<br>\n";
-$rets->Disconnect();
+  }
+
+  echo "+ Disconnecting\n";
+  $rets->Disconnect();
+}
 ?>
